02c83ff725bbc1c35c1ced9bec3aea323e3142e7
Carsten Ringe <carsten@kopis.de>
2012-08-23 06:22:45 +0200

    Prepare POM for AWS dependency when it becomes available.

--- src/main/java/de/kopis/glacier/CommandLineGlacierUploader.java
+++ src/main/java/de/kopis/glacier/CommandLineGlacierUploader.java
@@ -10,15 +10,24 @@ import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
 
 public class CommandLineGlacierUploader {
 
-	public static void main(final String[] args) throws IOException {
-		final AWSCredentials credentials = new PropertiesCredentials(
-				CommandLineGlacierUploader.class.getResourceAsStream("/aws.properties"));
-		final AmazonGlacierClient client = new AmazonGlacierClient(credentials);
-		client.setEndpoint("https://glacier.eu-west-1.amazonaws.com");    
-		final ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials);
-		final File uploadFile = new File(args[0]);
-		final String archiveId = atm.upload("cr_backup", "archive-" + uploadFile.getName(),
-				uploadFile).getArchiveId();
-		System.out.println("Uploaded archive " + archiveId);
-	}
+  public static void main(final String[] args) throws IOException {
+    if (args.length != 1) {
+      System.err
+          .println("You have to provide a path to an archive file as the only parameter to this application.");
+    }
+
+    final File uploadFile = new File(args[0]);
+
+    final AWSCredentials credentials = new PropertiesCredentials(
+        CommandLineGlacierUploader.class.getResourceAsStream("/aws.properties"));
+
+    final AmazonGlacierClient client = new AmazonGlacierClient(credentials);
+    client.setEndpoint("https://glacier.eu-west-1.amazonaws.com");
+
+    final ArchiveTransferManager atm = new ArchiveTransferManager(client,
+        credentials);
+    final String archiveId = atm.upload("cr_backup",
+        "archive-" + uploadFile.getName(), uploadFile).getArchiveId();
+    System.out.println("Uploaded archive " + archiveId);
+  }
 }
