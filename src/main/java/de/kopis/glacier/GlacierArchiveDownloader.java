65637be5bcde455ab82c244ace5a93b461b970b1
Carsten Ringe <carsten@kopis.de>
2012-08-31 12:02:51 +0200

    Try to do a quick fix of issue #13.

--- src/main/java/de/kopis/glacier/GlacierArchiveDownloader.java
+++ src/main/java/de/kopis/glacier/GlacierArchiveDownloader.java
@@ -30,33 +30,43 @@ import java.net.URL;
 
 import com.amazonaws.services.glacier.model.DeleteArchiveRequest;
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sqs.AmazonSQSClient;
 
 public class GlacierArchiveDownloader extends AbstractGlacierCommand {
 
-  public GlacierArchiveDownloader(final File credentials) throws IOException {
-    super(credentials);
-  }
-
-  public void download(final URL endpointUrl, final String vaultName, final String archiveId) {
-    System.out.println("Downloading archive " + archiveId + " from vault " + vaultName + "...");
-    client.setEndpoint(endpointUrl.toExternalForm());
-
-    try {
-      final File downloadFile = File.createTempFile("glacier-", ".dl");
-      final ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials);
-      atm.download(vaultName, archiveId, downloadFile);
-      System.out.println("Archive downloaded to " + downloadFile);
-    } catch (final IOException e) {
-      System.err.println("Can not download archive " + archiveId + " from vault " + vaultName + ".");
-      e.printStackTrace();
+    public GlacierArchiveDownloader(final File credentials) throws IOException {
+        super(credentials);
     }
-  }
 
-  public void delete(final URL endpointUrl, final String vaultName, final String archiveId) {
-    System.out.println("Deleting archive " + archiveId + " from vault " + vaultName + "...");
-    final DeleteArchiveRequest deleteArchiveRequest = new DeleteArchiveRequest(vaultName, archiveId);
-    client.deleteArchive(deleteArchiveRequest);
-    System.out.println("Archive " + archiveId + " deleted from vault " + vaultName + ".");
-  }
+    public void download(final URL endpointUrl, final String vaultName, final String archiveId) {
+        System.out.println("Downloading archive " + archiveId + " from vault " + vaultName + "...");
+        client.setEndpoint(endpointUrl.toExternalForm());
+
+        // TODO is this fixing issue #13?
+        final AmazonSQSClient sqs = new AmazonSQSClient(credentials);
+        final AmazonSNSClient sns = new AmazonSNSClient(credentials);
+        sqs.setEndpoint(endpointUrl.toExternalForm().replaceAll("glacier", "sqs"));
+        sns.setEndpoint(endpointUrl.toExternalForm().replaceAll("glacier", "sns"));
+
+        try {
+            final File downloadFile = File.createTempFile("glacier-", ".dl");
+
+            ArchiveTransferManager atm = new ArchiveTransferManager(client, sqs, sns);
+
+            atm.download(vaultName, archiveId, downloadFile);
+            System.out.println("Archive downloaded to " + downloadFile);
+        } catch (final IOException e) {
+            System.err.println("Can not download archive " + archiveId + " from vault " + vaultName + ".");
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(final URL endpointUrl, final String vaultName, final String archiveId) {
+        System.out.println("Deleting archive " + archiveId + " from vault " + vaultName + "...");
+        final DeleteArchiveRequest deleteArchiveRequest = new DeleteArchiveRequest(vaultName, archiveId);
+        client.deleteArchive(deleteArchiveRequest);
+        System.out.println("Archive " + archiveId + " deleted from vault " + vaultName + ".");
+    }
 
 }
