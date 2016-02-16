759de1c6cb08774b1f5f2ae7457ca42fa9eeecb6
Carsten <carsten@kopis.de>
2016-02-16 20:40:31 +0100

    Use BigDecimal to calculate HumanReadableSize.

--- src/main/java/de/kopis/glacier/printers/HumanReadableSize.java
+++ src/main/java/de/kopis/glacier/printers/HumanReadableSize.java
@@ -1,8 +1,11 @@
 package de.kopis.glacier.printers;
 
+import org.apache.log4j.Logger;
+
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -33,68 +36,77 @@ import java.util.regex.Pattern;
  */
 
 public class HumanReadableSize {
-	
-  public static String parse(final Long size) throws IllegalArgumentException {
-	    return parse(size.toString());
-  }
-	
-  public static String parse(final Integer size) throws IllegalArgumentException {
-    return parse(size.toString());
-  }
+    private static final Logger LOG = Logger.getLogger(HumanReadableSize.class);
 
-  public static String parse(final String size) throws IllegalArgumentException {
-    final String[] sanitizedSize = sanitize(size);
-    double sizeAsDouble = 0;
-    try {
-      // parse as US value, because WTF? java default?
-      sizeAsDouble = NumberFormat.getInstance(Locale.US).parse(sanitizedSize[0]).doubleValue();
-    } catch (final ParseException e) {
-      throw new IllegalArgumentException("Can not parse Number", e);
-    }
-    String humanReadableSize = "";
-    String sizeClass = sanitizedSize[1];
-    if (sizeAsDouble >= 1024) {
-      sizeClass = getLargerSizeClass(sanitizedSize[1]);
-      humanReadableSize = parse(sizeAsDouble / 1024.0 + " " + sizeClass);
-    } else {
-      humanReadableSize = round(Double.toString(sizeAsDouble), 2, BigDecimal.ROUND_UP) + sizeClass;
-    }
-    return humanReadableSize;
-  }
-
-  private static String getLargerSizeClass(final String oldSizeClass) {
-    String newSizeClass = "B";
-    if ("B".equals(oldSizeClass)) {
-      newSizeClass = "kB";
-    } else if ("kB".equals(oldSizeClass)) {
-      newSizeClass = "MB";
-    } else if ("MB".equals(oldSizeClass)) {
-      newSizeClass = "GB";
-    } else if ("GB".equals(oldSizeClass)) {
-      newSizeClass = "TB";
-    } else if ("TB".equals(oldSizeClass)) {
-      newSizeClass = "PT";
-    }
-    return newSizeClass;
-  }
-
-  public static String[] sanitize(final String size) {
-    final Pattern patternClass = Pattern.compile("([0-9.]+)\\s*?([kMGTP]?B)");
-    final Matcher m = patternClass.matcher(size);
-    String[] s = new String[] { size, "B" };
-    if (m.find()) {
-      final String pureSize = m.group(1);
-      final String sizeClass = m.group(2);
-      s = new String[] { pureSize, sizeClass };
+    public static String parse(final Long size) throws IllegalArgumentException {
+        return parse(size.toString());
     }
 
-    return s;
-  }
+    public static String parse(final Integer size) throws IllegalArgumentException {
+        return parse(size.toString());
+    }
 
-  private static String round(final String unrounded, final int precision, final int roundingMode) {
-    final BigDecimal bd = new BigDecimal(unrounded);
-    final BigDecimal rounded = bd.setScale(precision, roundingMode);
-    return rounded.toString();
-  }
+    public static String parse(final String size) throws IllegalArgumentException {
+        LOG.debug("Parsing '" + size + "'");
+        final String[] sanitizedSize = sanitize(size);
+        BigDecimal sizeAsNumber = null;
+        try {
+            // parse as US value, because WTF? java default?
+            Number parsed = NumberFormat.getInstance(Locale.US).parse(sanitizedSize[0]);
+            sizeAsNumber = new BigDecimal(parsed.toString());
+        } catch (final ParseException e) {
+            throw new IllegalArgumentException("Can not parse Number", e);
+        }
+        LOG.debug("Parsed as number: " + sizeAsNumber);
+        String humanReadableSize = "";
+        String sizeClass = sanitizedSize[1];
+        if (sizeAsNumber.longValue() >= 1024L) {
+            sizeClass = getLargerSizeClass(sanitizedSize[1]);
+            humanReadableSize = parse(sizeAsNumber.divide(new BigDecimal("1024")) + " " + sizeClass);
+        } else {
+            humanReadableSize = round(new BigDecimal(sizeAsNumber.toString()), 2, BigDecimal.ROUND_UP) + sizeClass;
+        }
+        LOG.debug("Parsed: " + humanReadableSize);
+        return humanReadableSize;
+    }
 
+    private static String getLargerSizeClass(final String oldSizeClass) {
+        String newSizeClass = "B";
+        if ("B".equals(oldSizeClass)) {
+            newSizeClass = "kB";
+        } else if ("kB".equals(oldSizeClass)) {
+            newSizeClass = "MB";
+        } else if ("MB".equals(oldSizeClass)) {
+            newSizeClass = "GB";
+        } else if ("GB".equals(oldSizeClass)) {
+            newSizeClass = "TB";
+        } else if ("TB".equals(oldSizeClass)) {
+            newSizeClass = "PT";
+        }
+        LOG.debug("previous sizeClass: " + oldSizeClass + " new sizeClass: " + newSizeClass);
+        return newSizeClass;
+    }
+
+    public static String[] sanitize(final String size) {
+        LOG.debug("Sanitizing '" + size + "'");
+        final Pattern patternClass = Pattern.compile("([0-9.]+)\\s*?([kMGTP]?B)");
+        final Matcher m = patternClass.matcher(size);
+        String[] s = new String[]{size, "B"};
+        if (m.find()) {
+            final String pureSize = m.group(1);
+            final String sizeClass = m.group(2);
+            s = new String[]{pureSize, sizeClass};
+        }
+
+        LOG.debug("Sanitized: " + Arrays.deepToString(s));
+        return s;
+    }
+
+    private static String round(final BigDecimal unrounded, final int precision, final int roundingMode) {
+        LOG.debug("Rounding '" + unrounded + "' to " + precision + " precision");
+        final BigDecimal rounded = unrounded.setScale(precision, roundingMode);
+        LOG.debug("Rounded: " + rounded);
+        return rounded.toString();
+    }
 }
+
--- src/test/java/de/kopis/glacier/printers/HumanReadableSizeTest.java
+++ src/test/java/de/kopis/glacier/printers/HumanReadableSizeTest.java
@@ -1,6 +1,7 @@
 package de.kopis.glacier.printers;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -30,33 +31,48 @@ import org.junit.Test;
  */
 
 public class HumanReadableSizeTest {
-  @Test
-  public void sanitizeMissingSizeIndicator() {
-    assertArrayEquals(new String[] { "123456789", "B" }, HumanReadableSize.sanitize("123456789"));
-  }
+    @Test
+    public void sanitizeMissingSizeIndicator() {
+        assertArrayEquals(new String[]{"123456789", "B"}, HumanReadableSize.sanitize("123456789"));
+    }
 
-  @Test
-  public void sanitizeBytes() {
-    assertArrayEquals(new String[] { "123456789", "B" }, HumanReadableSize.sanitize("123456789 B"));
-  }
+    @Test
+    public void sanitizeBytes() {
+        assertArrayEquals(new String[]{"123456789", "B"}, HumanReadableSize.sanitize("123456789 B"));
+    }
 
-  @Test
-  public void sanitizeKilobytes() {
-    assertArrayEquals(new String[] { "123456789", "kB" }, HumanReadableSize.sanitize("123456789kB"));
-  }
+    @Test
+    public void sanitizeKilobytes() {
+        assertArrayEquals(new String[]{"123456789", "kB"}, HumanReadableSize.sanitize("123456789kB"));
+    }
 
-  @Test
-  public void sanitizeMegabytes() {
-    assertArrayEquals(new String[] { "123456789", "MB" }, HumanReadableSize.sanitize("123456789MB"));
-  }
+    @Test
+    public void sanitizeMegabytes() {
+        assertArrayEquals(new String[]{"123456789", "MB"}, HumanReadableSize.sanitize("123456789MB"));
+    }
 
-  @Test
-  public void sanitizeGigabytes() {
-    assertArrayEquals(new String[] { "123456789", "GB" }, HumanReadableSize.sanitize("123456789	 GB"));
-  }
+    @Test
+    public void sanitizeGigabytes() {
+        assertArrayEquals(new String[]{"123456789", "GB"}, HumanReadableSize.sanitize("123456789	 GB"));
+    }
 
-  @Test
-  public void sanitizeKilTerabytes() {
-    assertArrayEquals(new String[] { "123456789", "TB" }, HumanReadableSize.sanitize("123456789TB"));
-  }
+    @Test
+    public void sanitizeKilTerabytes() {
+        assertArrayEquals(new String[]{"123456789", "TB"}, HumanReadableSize.sanitize("123456789TB"));
+    }
+
+    @Test
+    public void terabyteLargeSizeFailed() {
+        assertEquals("68.44TB", HumanReadableSize.parse("75240135239680"));
+    }
+
+    @Test
+    public void gigabyteLargeSizeFailed() {
+        assertEquals("68.44GB", HumanReadableSize.parse("73476694570"));
+    }
+
+    @Test
+    public void megabyteLargeSizeFailed() {
+        assertEquals("68.44MB", HumanReadableSize.parse("71754584"));
+    }
 }
