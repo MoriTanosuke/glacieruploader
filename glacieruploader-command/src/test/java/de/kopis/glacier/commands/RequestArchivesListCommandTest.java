a1c4afb0f5f24269498c8711d084a1323cc7ce7c
Carsten <carsten@kopis.de>
2017-04-13 11:27:57 +0200

    Add missing license headers to new files

--- glacieruploader-command/src/test/java/de/kopis/glacier/commands/RequestArchivesListCommandTest.java
+++ glacieruploader-command/src/test/java/de/kopis/glacier/commands/RequestArchivesListCommandTest.java
@@ -1,5 +1,27 @@
 package de.kopis.glacier.commands;
 
+/*-
+ * #%L
+ * glacieruploader-command
+ * %%
+ * Copyright (C) 2012 - 2017 Carsten Ringe
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.isA;
